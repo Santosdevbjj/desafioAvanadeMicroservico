9412b8bb77aad64d21748fbf9e1ce7c0cbb48769
Sergio <santossergiorealbjj@outlook.com>
2025-09-08 10:08:45 -0300

    Create IProdutoRepository.cs

---
+++ EstoqueService/Repositories/IProdutoRepository.cs
@@ -0,0 +1,14 @@
+using EstoqueService.Models;
+
+namespace EstoqueService.Repositories
+{
+    public interface IProdutoRepository
+    {
+        Task<IEnumerable<Produto>> GetAllAsync();
+        Task<Produto?> GetByIdAsync(int id);
+        Task AddAsync(Produto produto);
+        Task UpdateAsync(Produto produto);
+        Task DeleteAsync(int id);
+        Task SaveChangesAsync();
+    }
+}
