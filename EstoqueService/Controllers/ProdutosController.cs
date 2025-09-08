722d27d708c9ac735b7d15d238be8bc3d32fe19f
Sergio <santossergiorealbjj@outlook.com>
2025-09-08 01:01:40 -0300

    Update ProdutosController.cs

--- EstoqueService/Controllers/ProdutosController.cs
+++ EstoqueService/Controllers/ProdutosController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using EstoqueService.Models;
-using EstoqueService.Repositories;
+using EstoqueService.Services;
 
 namespace EstoqueService.Controllers;
 
@@ -8,21 +8,44 @@ namespace EstoqueService.Controllers;
 [Route("api/[controller]")]
 public class ProdutosController : ControllerBase
 {
-    private readonly ProdutoRepository _repository;
+    private readonly ProdutoService _service;
 
-    public ProdutosController(ProdutoRepository repository)
+    public ProdutosController(ProdutoService service)
     {
-        _repository = repository;
+        _service = service;
     }
 
     [HttpGet]
     public async Task<IActionResult> GetAll() =>
-        Ok(await _repository.GetAll());
+        Ok(await _service.GetAll());
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var produto = await _service.GetById(id);
+        if (produto == null) return NotFound();
+        return Ok(produto);
+    }
 
     [HttpPost]
     public async Task<IActionResult> Create([FromBody] Produto produto)
     {
-        await _repository.Add(produto);
-        return CreatedAtAction(nameof(GetAll), new { id = produto.Id }, produto);
+        await _service.Add(produto);
+        return CreatedAtAction(nameof(GetById), new { id = produto.Id }, produto);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(int id, [FromBody] Produto produto)
+    {
+        if (id != produto.Id) return BadRequest();
+        await _service.Update(produto);
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        await _service.Delete(id);
+        return NoContent();
     }
 }
