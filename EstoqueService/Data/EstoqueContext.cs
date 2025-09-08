aec25a702f3ed1fa44bd9ab1b514695aca9f78db
Sergio <santossergiorealbjj@outlook.com>
2025-09-08 00:47:54 -0300

    Update EstoqueContext.cs

--- EstoqueService/Data/EstoqueContext.cs
+++ EstoqueService/Data/EstoqueContext.cs
@@ -6,5 +6,6 @@ namespace EstoqueService.Data;
 public class EstoqueContext : DbContext
 {
     public EstoqueContext(DbContextOptions<EstoqueContext> options) : base(options) { }
+
     public DbSet<Produto> Produtos { get; set; }
 }
