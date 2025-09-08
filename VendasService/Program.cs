067b6e3b8aedd47cbb090cc01b11a4c9d8779a46
Sergio <santossergiorealbjj@outlook.com>
2025-09-08 01:51:06 -0300

    Create Program.cs

---
+++ VendasService/Program.cs
@@ -0,0 +1,23 @@
+using VendasService.Services;
+using VendasService.Messaging;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<RabbitMqPublisher>();
+builder.Services.AddSingleton<PedidoService>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.Run();
