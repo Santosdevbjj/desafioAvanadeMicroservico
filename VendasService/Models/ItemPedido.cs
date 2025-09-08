07b0098550f0371ca3d54e714225424cf5282fa4
Sergio <santossergiorealbjj@outlook.com>
2025-09-08 02:05:34 -0300

    Update ItemPedido.cs

--- VendasService/Models/ItemPedido.cs
+++ VendasService/Models/ItemPedido.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace VendasService.Models;
 
 public class ItemPedido
 {
+    [Key]
     public int Id { get; set; }
     public int ProdutoId { get; set; }
     public int Quantidade { get; set; }
+
+    // Chave estrangeira
     public int PedidoId { get; set; }
 }
+
