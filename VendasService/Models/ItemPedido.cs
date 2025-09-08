73debc4a9873c1b07840db8f9d8b4856963fb055
Sergio <santossergiorealbjj@outlook.com>
2025-09-08 01:39:04 -0300

    Create ItemPedido.cs

---
+++ VendasService/Models/ItemPedido.cs
@@ -0,0 +1,9 @@
+namespace VendasService.Models;
+
+public class ItemPedido
+{
+    public int Id { get; set; }
+    public int ProdutoId { get; set; }
+    public int Quantidade { get; set; }
+    public int PedidoId { get; set; }
+}
