663ff74f0959f38ec029872bea7f3c26d68aa1d3
Sergio <santossergiorealbjj@outlook.com>
2025-09-08 10:04:18 -0300

    Update EstoqueContext.cs

--- EstoqueService/Data/EstoqueContext.cs
+++ EstoqueService/Data/EstoqueContext.cs
@@ -1,11 +1,27 @@
-using Microsoft.EntityFrameworkCore;
 using EstoqueService.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace EstoqueService.Data;
-
-public class EstoqueContext : DbContext
+namespace EstoqueService.Data
 {
-    public EstoqueContext(DbContextOptions<EstoqueContext> options) : base(options) { }
+    public class EstoqueContext : DbContext
+    {
+        public EstoqueContext(DbContextOptions<EstoqueContext> options) : base(options) { }
 
-    public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produto>()
+                .HasKey(p => p.Id);
+
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.Nome)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.Quantidade)
+                .IsRequired();
+        }
+    }
 }
