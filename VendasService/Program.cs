a12e2683d42a49098a737b87987f8e6c237e18a9
Sergio <santossergiorealbjj@outlook.com>
2025-09-08 16:15:30 -0300

    Update Program.cs

--- VendasService/Program.cs
+++ VendasService/Program.cs
@@ -9,9 +9,12 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configuração do banco de dados
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Configuração do banco de dados (MySQL)
+builder.Services.AddDbContext<VendasContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+    ));
 
 // Repositórios e serviços
 builder.Services.AddScoped<IPedidoRepository, PedidoRepository>();
@@ -21,33 +24,41 @@ builder.Services.AddScoped<PedidoService>();
 builder.Services.AddScoped<JwtTokenService>();
 
 // Configuração de autenticação JWT
-var jwtKey = builder.Configuration["Jwt:Key"] ?? "chave_super_secreta_123";
-var jwtIssuer = builder.Configuration["Jwt:Issuer"] ?? "AvanadeAuth";
+var jwtKey = builder.Configuration["Jwt:Key"];
+var jwtIssuer = builder.Configuration["Jwt:Issuer"];
 
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
     {
-        ValidateIssuer = true,
-        ValidateAudience = false,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = jwtIssuer,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
-    };
-});
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = false, // sem validação de Audience nesse exemplo
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtIssuer,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey!))
+        };
+    });
+
+builder.Services.AddAuthorization();
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Swagger somente em dev
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseRouting();
 
-// Ativa autenticação/autorização
+// Middleware de autenticação/autorização
 app.UseAuthentication();
 app.UseAuthorization();
 
