ce42641a4834658c3ab0d0c28f30a6de3a9103bb
Sergio <santossergiorealbjj@outlook.com>
2025-09-08 00:38:46 -0300

    Create Program.cs

---
+++ EstoqueService/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using EstoqueService.Data;
+using EstoqueService.Repositories;
+using EstoqueService.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configura MySQL
+builder.Services.AddDbContext<EstoqueContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("MySQL"),
+    new MySqlServerVersion(new Version(8, 0, 32))));
+
+// Injeta dependências
+builder.Services.AddScoped<ProdutoRepository>();
+builder.Services.AddScoped<ProdutoService>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
