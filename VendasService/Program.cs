010c1b58091d0d4bf8fa2087565d2b91266dc033
Sergio <santossergiorealbjj@outlook.com>
2025-09-08 02:17:37 -0300

    Update Program.cs

--- VendasService/Program.cs
+++ VendasService/Program.cs
@@ -1,10 +1,21 @@
+using Microsoft.EntityFrameworkCore;
 using VendasService.Services;
 using VendasService.Messaging;
+using VendasService.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Banco MySQL
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("MySqlConnection"),
+        new MySqlServerVersion(new Version(8, 0, 29))
+    )
+);
+
+// Injeção de dependências
 builder.Services.AddSingleton<RabbitMqPublisher>();
-builder.Services.AddSingleton<PedidoService>();
+builder.Services.AddScoped<PedidoService>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -21,3 +32,4 @@ if (app.Environment.IsDevelopment())
 app.MapControllers();
 
 app.Run();
+
